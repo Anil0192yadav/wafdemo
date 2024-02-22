f660452f50f4a684e2b530ca9617c65e0471813a
Anil0192yadav <159400693+Anil0192yadav@users.noreply.github.com>
2024-02-22 23:03:27 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,97 +1,272 @@
+#####
+# IP set resources
+#####
+resource "aws_wafv2_ip_set" "custom_ip_set" {
+  name = "${var.name_prefix}-custom-ip-set"
 
-# Create VPC
-resource "aws_vpc" "my_vpc" {
-  cidr_block = var.vpc_cidr
-  enable_dns_support = true
-  enable_dns_hostnames = true
+  scope              = "REGIONAL"
+  ip_address_version = "IPV4"
+
+  addresses = [
+    "10.0.0.0/16",
+    "10.10.0.0/16"
+  ]
+}
+
+resource "aws_wafv2_regex_pattern_set" "bad_bots_user_agent" {
+  name        = "BadBotsUserAgent"
+  description = "Some bots regex pattern set example"
+  scope       = "REGIONAL"
+
+  regular_expression {
+    regex_string = "semrushbot|censysinspect"
+  }
+
+  regular_expression {
+    regex_string = "blackwidow|acunetix-*"
+  }
+
   tags = {
-    Name = var.vpc_name
+    Name        = "RegexBadBots"
+    Environment = "WAFv2"
   }
 }
- 
-# Create public subnet
-resource "aws_subnet" "public_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = var.public_subnet_cidr
-  availability_zone = var.availability_zone
-  map_public_ip_on_launch = true
+
+#####
+# Web Application Firewall configuration
+#####
+module "waf" {
+  source = "../.."
+
+  name_prefix = var.name_prefix
+
+  allow_default_action = true
+
+  scope = "REGIONAL"
+
+  create_alb_association = false
+
+  visibility_config = {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "${var.name_prefix}-waf-setup-waf-main-metrics"
+    sampled_requests_enabled   = false
+  }
+
+  rules = [
+    {
+      name     = "AWSManagedRulesCommonRuleSet-rule-1"
+      priority = "1"
+
+      override_action = "none"
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        metric_name                = "AWSManagedRulesCommonRuleSet-metric"
+        sampled_requests_enabled   = false
+      }
+
+      managed_rule_group_statement = {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    },
+    {
+      ### AND rule example
+      name     = "block-specific-uri-path-and-requests-from-nl-gb-and-us"
+      priority = 2
+      action   = "block"
+
+      and_statement = {
+        statements = [ # 2 or more statements are required for AND
+          {
+            byte_match_statement = {
+              field_to_match = {
+                uri_path = "{}"
+              }
+
+              positional_constraint = "STARTS_WITH"
+              search_string         = "/path/to/match"
+              priority              = 0
+              type                  = "NONE"
+            }
+          },
+          {
+            geo_match_statement = {
+              country_codes = ["NL", "GB", "US"]
+            }
+          }
+        ]
+      }
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        sampled_requests_enabled   = false
+      }
+    },
+    {
+      ### OR rule example
+      name     = "block-specific-ip-set-or-body-contains-hotmail"
+      priority = 3
+      action   = "block"
+
+      or_statement = {
+        statements = [ # 2 or more statements are required for OR
+          {
+            ip_set_reference_statement = {
+              arn = aws_wafv2_ip_set.custom_ip_set.arn
+            }
+          },
+          {
+            byte_match_statement = {
+              field_to_match = {
+                body = "{}"
+              }
+
+              positional_constraint = "CONTAINS"
+              search_string         = "@hotmail.com"
+              priority              = 0
+              type                  = "NONE"
+            }
+          }
+        ]
+      }
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        sampled_requests_enabled   = false
+      }
+    },
+    {
+      ### AND rule example with NOT statement
+      name     = "block-specific-uri-path-and-not-requests-from-nl-gb-and-us"
+      priority = 4
+      action   = "block"
+
+      and_statement = {
+        statements = [ # 2 or more statements are required for AND
+          {
+            byte_match_statement = {
+              field_to_match = {
+                uri_path = "{}"
+              }
+
+              positional_constraint = "STARTS_WITH"
+              search_string         = "/path/to/match"
+              priority              = 0
+              type                  = "NONE"
+            }
+          },
+          {
+            geo_match_statement = {
+              country_codes = ["NL", "GB", "US"]
+            }
+          },
+          {
+            not_statement = {
+              regex_pattern_set_reference_statement = {
+                arn = aws_wafv2_regex_pattern_set.bad_bots_user_agent.arn
+                field_to_match = {
+                  single_header = {
+                    name = "user-agent"
+                  }
+                }
+
+                priority = 0
+                type     = "LOWERCASE" # The text transformation type
+              }
+            }
+          },
+        ]
+      }
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        sampled_requests_enabled   = false
+      }
+    },
+
+    {
+      #### AND rule with OR statement
+      name     = "block-specific-uri"
+      priority = 5
+      action   = "block"
+
+      rate_based_statement = {
+        aggregate_key_type = "IP"
+        limit              = 123
+
+        scope_down_statement = {
+          and_statement = {
+            statements = [
+              {
+                or_statement = {
+                  statements = [
+                    {
+                      byte_match_statement = {
+                        positional_constraint = "EXACTLY"
+                        search_string         = "some.url.com"
+
+                        field_to_match = {
+
+                          single_header = {
+                            name = "host"
+                          }
+                        }
+
+                        priority = 0
+                        type     = "NONE"
+                      }
+
+                    },
+                    {
+                      byte_match_statement = {
+                        positional_constraint = "EXACTLY"
+                        search_string         = "some.other.url.com"
+
+                        field_to_match = {
+                          single_header = {
+                            name = "host"
+                          }
+                        }
+
+                        priority = 0
+                        type     = "NONE"
+                      }
+
+                    }
+                  ]
+                }
+              },
+              {
+                byte_match_statement = {
+                  positional_constraint = "STARTS_WITH"
+                  search_string         = "/whatever"
+
+                  field_to_match = {
+
+                    uri_path = "{}"
+                  }
+
+                  priority = 0
+                  type     = "NONE"
+                }
+              }
+
+            ]
+          }
+        }
+      }
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        sampled_requests_enabled   = false
+        metric_name                = "whatever-metric"
+
+      }
+    }
+  ]
+
   tags = {
-    Name = var.public_subnet_name
+    "Environment" = "test"
   }
-}
- 
-# Create internet gateway
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
-}
- 
-# Create route table
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
- 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw.id
-  }
- 
-  tags = {
-    Name = var.public_route_table_name
-  }
-}
- 
-# Associate public subnet with public route table
-resource "aws_route_table_association" "public_route_association" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_route_table.id
-}
- 
-# Create security group
-resource "aws_security_group" "my_security_group" {
-  name        = var.security_group_name
-  description = "Allow inbound traffic for ports 80, 22, 443 and any outbound traffic"
- 
-  vpc_id = aws_vpc.my_vpc.id
- 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
- 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
- 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
- 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
- 
-# Create EC2 instance
-resource "aws_instance" "my_ec2_instance" {
-  ami           = var.ec2_ami
-  instance_type = var.ec2_instance_type
-  subnet_id     = aws_subnet.public_subnet.id
-  security_groups = [aws_security_group.my_security_group.name]
- 
-  tags = {
-    Name = var.ec2_instance_name
-  }
- 
-  # Allocate public IP
-  associate_public_ip_address = true
 }
